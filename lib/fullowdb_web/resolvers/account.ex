b3552f1f64f8eb834112167b5e3aacd4ee474c52
Fr4nz909 <franz.zetzsch@gmail.com>
2020-07-17 13:53:00 +0000

    More Ordering Logic + Login Logic

--- lib/fullowdb_web/resolvers/account.ex
+++ lib/fullowdb_web/resolvers/account.ex
@@ -1,5 +1,17 @@
 defmodule FullowdbWeb.Resolvers.Account do
     alias Fullowdb.Account
+
+    def login(_, %{email: email, password: password, role: role}, _) do
+        case Accounts.authenticate(role, email, password) do
+          {:ok, user} ->
+            token = Fullowdb.Authentication.sign(%{
+              role: role, id: user.id
+            })
+            {:ok, %{token: token, user: user}}
+          _ ->
+            {:error, "incorrect email or password"}
+        end
+      end
     
     def list_users(_, args, %{context: context}) do
         IO.inspect(context)
@@ -19,4 +31,12 @@ defmodule FullowdbWeb.Resolvers.Account do
         end
     end
 
+    def me(_, _, %{context: %{current_user: current_user}}) do
+        {:ok, current_user}
+      end
+      def me(_, _, _) do
+        {:ok, nil}
+      end
+    
+    end
 end
---
+++ lib/fullowdb_web/resolvers/ordering.ex
@@ -0,0 +1,33 @@
+defmodule FullowdbWeb.Resolvers.Ordering do
+    alias FullowdbWeb.Ordering
+  
+    def ready_order(_, %{id: id}, _) do
+      order = Ordering.get_order!(id)
+      with {:ok, order} <- Ordering.update_order(order, %{state: "ready"}) do
+        {:ok, %{order: order}}
+      end
+    end
+  
+    def complete_order(_, %{id: id}, _) do
+      order = Ordering.get_order!(id)
+  
+      with {:ok, order} <- Ordering.update_order(order, %{state: "complete"}) do
+        {:ok, %{order: order}}
+      end
+    end
+  
+    def place_order(_, %{input: place_order_input}, %{context: context}) do
+      place_order_input = case context[:current_user] do
+        %{role: "customer", id: id} ->
+          Map.put(place_order_input, :customer_id, id)
+        _ ->
+          place_order_input
+      end
+      with {:ok, order} <- Ordering.create_order(place_order_input) do
+        Absinthe.Subscription.publish(FullowdbWeb.Endpoint, order,
+          new_order: [order.customer_id, "*"]
+        )
+        {:ok, %{order: order}}
+      end
+    end
+  end
